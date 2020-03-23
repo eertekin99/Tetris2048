60aa91c1dd0df7746ce007c13140d7420645eb75
Yasin Yılmaz <47917050+yasin0072@users.noreply.github.com>
2020-03-23 16:55:29 +0300

    Add files via upload

--- Shape.java
+++ Shape.java
@@ -2,149 +2,168 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Shape {
-    int values [];
-    int coord[][];
-    int boardY;
-    int boardX;
-    private boolean end=true;
+	int values [];
+	int coord[][];
+	int boardY;
+	int boardX;
+	private boolean end=true;
 
 
+	public Shape(int[][] coord,int boardX,int boardY,int[] values){
+		this.coord=coord;
+		this.boardX=boardX;
+		this.boardY=boardY;
+		this.values=values;
 
-    public Shape(int[][] coord,int boardX,int boardY,int[] values){
-        this.coord=coord;
-        this.boardX=boardX;
-        this.boardY=boardY;
-        this.values=values;
+	}
 
-    }
+	boolean move(int [][]board){
 
-    boolean move(int [][]board){
-        if (isSuit(board)){
-            for (int i = 0; i < coord.length; i++) {
-                coord[i][1]+=1;
+		if (isSuit(board)){
+			for (int i = 0; i < coord.length; i++) {
+				coord[i][1]+=1;
 
-            }
-//            System.out.println(Arrays.toString(coord[0]));
-//            System.out.println(Arrays.toString(coord[1]));
-//            System.out.println(Arrays.toString(coord[2]));
-//            System.out.println(Arrays.toString(coord[3]));
-//            System.out.println();
-            return false;
-        }
-        else{
-            done(board);
-//            System.out.println(Arrays.toString(coord[0]));
-//            System.out.println(Arrays.toString(coord[1]));
-//            System.out.println(Arrays.toString(coord[2]));
-//            System.out.println(Arrays.toString(coord[3]));
-//            System.out.println();
-            return true;
-        }
-    }
-    boolean isSuit(int [][] board){
-        for(int i = 0; i < coord.length;i++){
-            if(coord[i][0]<0|| coord[i][1]<0){
-                continue;
-            }
-            if( coord[i][1]+1>board.length-1)
-                return false;
+			}
+			return false;
+		}
+		else{
+			done(board);
+			return true;
+		}
+	}
+	boolean isSuit(int [][] board){
+		for(int i = 0; i < coord.length;i++){
+			if(coord[i][0]<0|| coord[i][1]<0){
+				continue;
+			}
+			if( coord[i][1]+1>board.length-1)
+				return false;
 
-            if(board[coord[i][1]+1][coord[i][0]]!=0)
-                return false ;
-        }
-        return true;
-    }
-    void done(int [][]board){
-        if(!lose()) {
-            for (int i = 0; i < coord.length; i++) {
-                board[coord[i][1]][coord[i][0]] = values[i];
-            }
-        }
-        else {
-            end=false;
-        }
+			if(board[coord[i][1]+1][coord[i][0]]!=0)
+				return false ;
+		}
+		return true;
+	}
+	void done(int [][]board){
+		if(!lose()) {
+			for (int i = 0; i < coord.length; i++) {
+				board[coord[i][1]][coord[i][0]] = values[i];
+			}
+		}
+		else {
 
-    }
-    void moveRight(){
-        for (int i = 0; i < coord.length; i++) {
-            coord[i][0]+=1;
-        }
-    }
-    void moveLeft(){
-        if(!outOfBorder()){
-            for (int i = 0; i < coord.length; i++) {
-                coord[i][0]-=1;
-            }
-        }
-    }
-    boolean outOfBorder(){
-        for(int i = 0; i < coord.length;i++){
-            if(coord[i][0]<0||coord[i][1]>boardY-1){
-                return true;
-            }
-        }
-        return false;
-    }
-    boolean lose(){
-        for (int i = 0; i < coord.length; i++) {
-            if(coord[i][1]<0){
-                return true;
-            }
-        }
-        return false;
-    }
+			end=false;
+		}
 
-    void rotateRight(){
+	}
 
-    }
-    void rotateLeft(){
+	void moveRight(int[][] board){
+		
 
-    }
+			if(!greaterThanX(board))
+				for (int i = 0; i < coord.length; i++)
+					
+						coord[i][0]+=1;
+	
+	}
+	void moveLeft(int[][]board){
+		if(!lessThanX(board)){
+			for (int i = 0; i < coord.length; i++) {
+				coord[i][0]-=1;
+			}
+		}
+	}
 
-    boolean getEnd() {
-        return end;
-    }
-    int [][] review(){
-        int [][] arr = new int [4][2];
-        int min = 0;
-        int min2=20;
-        for (int i = 0; i < coord.length; i++) {
-            if(coord[i][1]<min){
-                min=coord[i][1];
-            }
-        }
-        for (int i = 0; i < coord.length; i++) {
-            if(coord[i][0]<min2){
-                min2=coord[i][0];
-            }
-        }
-        min=min*-1;
-        //System.out.println(min2);
-        for (int i = 0; i < arr.length; i++) {
-            arr[i][0]=coord[i][0]-min2;
-            arr[i][1]=min+coord[i][1];
-        }
-        int [][] rew = new int [2][4];
-        for (int i = 0; i <arr.length ; i++) {
-            rew[arr[i][1]][arr[i][0]]=values[i];
-        }
-        return rew;
-    }
+	boolean lessThanX(int[][] board){
+		for(int i = 0; i < coord.length;i++){
+			if(coord[i][0]-1<0){
+				return true;
+			}
+			
+			if(coord[i][1]-1<0){
+				continue;
+			}
+			
+			if(board[coord[i][1]][coord[i][0]-1]!=0){
+				return true;
+			}
+		}
+		return false;
+	}
 
-    void printReview(){
-        int [][] arr=review();
-        System.out.println("Next shape is: ");
+	boolean greaterThanX(int[][] board){
+		for(int i = 0; i < coord.length;i++){
+			if(coord[i][0]+1>boardX-1 ){
+				return true;
+			}
 
-        for (int i = 0; i <arr.length ; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(" "+arr[i][j]+" ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
+			if(coord[i][1]<0) {
+				continue;
+			}
+			if(board[coord[i][1]][coord[i][0]+1]!=0 ){
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public int[][] getCoord() {
-        return coord;
-    }
+	boolean lose(){
+		for (int i = 0; i < coord.length; i++) {
+			if(coord[i][1]<0){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void rotateRight(){
+
+	}
+	void rotateLeft(){
+
+	}
+
+	boolean getEnd() {
+		return end;
+	}
+	int [][] review(){
+		int [][] arr = new int [4][2];
+		int min = 0;
+		int min2=20;
+		for (int i = 0; i < coord.length; i++) {
+			if(coord[i][1]<min){
+				min=coord[i][1];
+			}
+		}
+		for (int i = 0; i < coord.length; i++) {
+			if(coord[i][0]<min2){
+				min2=coord[i][0];
+			}
+		}
+		min=min*-1;
+		//System.out.println(min2);
+		for (int i = 0; i < arr.length; i++) {
+			arr[i][0]=coord[i][0]-min2;
+			arr[i][1]=min+coord[i][1];
+		}
+		int [][] rew = new int [2][4];
+		for (int i = 0; i <arr.length ; i++) {
+			rew[arr[i][1]][arr[i][0]]=values[i];
+		}
+		return rew;
+	}
+
+	void printReview(){
+		int [][] arr=review();
+		for (int i = 0; i <arr.length ; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+	public int[][] getCoord() {
+		return coord;
+	}
 }
