36ac979ae82c4ec4749e002ed7bce586827077ee
Efe Ertekin <47754084+eertekin99@users.noreply.github.com>
2020-04-12 17:49:07 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,13 +4,13 @@ import java.awt.event.KeyEvent;
 /**
  * @author Burak Bahir Günden (041701027) , Efe Ertekin (041701006) , Yasin Yılmaz (041701020)
  * @since March 9, 2020
- * Firstly the program creates the game board and an integer array which contains special Tetris shapes.
- * These shapes made out of four boxes. Each box has an integer value. These values can be 2 or 4.
- * On the game board, the current shape moves from top to bottom and the next shape is shown in the bottom right.
+ * Firstly the program creates the game grid and an integer array which contains special Tetris tetrominos.
+ * These tetrominos made out of four boxes. Each box has an integer value. These values can be 2 or 4.
+ * On the game grid, the current tetromino moves from top to bottom and the next tetromino is shown in the bottom right.
  * The score of the player is shown in the top right. This game basically a combination of Tetris and 2048 games.
  * If the same numbers overlap then they merge and the merged number added to score.
  * If a line is completely full then that line be deleted and the sum of the numbers in deleted line added the score.
- * After that all other shapes move down.
+ * After that all other tetrominos move down.
  *
  */
 public class Main {
@@ -32,11 +32,11 @@ public class Main {
         drawLines();
         boolean x = true;
 
-        //playable board part
-        int board[][] = new int[12][8];
+        //playable grid part
+        int grid[][] = new int[12][8];
 
-        //all shapes we are using
-        int allShapes[][][] = {{{2, 0}, {3, 0}, {3, -1}, {4, -1}},
+        //all tetrominos we are using
+        int allTetrominos[][][] = {{{2, 0}, {3, 0}, {3, -1}, {4, -1}},
                 {{2, -1}, {3, -1}, {4, -1}, {5, -1}},
                 {{2, -1}, {3, 0}, {3, -1}, {4, 0}},
                 {{2, 0}, {3, 0}, {2, -1}, {3, -1}},
@@ -45,8 +45,8 @@ public class Main {
                 {{2, -1}, {3, -1}, {4, 0}, {4, -1}}
         };
 
-        //first shape created
-        Shape shape = randomShape(allShapes);
+        //first tetromino created
+        Tetromino tetromino = randomTetromino(allTetrominos);
 
         boolean game_on = true;
         while (game_on) {
@@ -60,20 +60,20 @@ public class Main {
             //restart option just for end of the game
             if (StdDraw.isKeyPressed(KeyEvent.VK_R)) {
                 //restart the game
-                restartProtocol(board);
+                restartProtocol(grid);
 
-                //first shape created for restarted game
-                shape = randomShape(allShapes);
+                //first tetromino created for restarted game
+                tetromino = randomTetromino(allTetrominos);
 
                 x = true;
             } else if(x) {
                 do {
 
-                    //creation of next shape
-                    Shape shapeNext = randomShape(allShapes);
+                    //creation of next tetrominos
+                    Tetromino tetrominoNext = randomTetromino(allTetrominos);
 
                     while (true) {
-                        if (shape.move(board)) {
+                        if (tetromino.move(grid)) {
                             break;
                         }
                         boolean check = true;
@@ -89,22 +89,22 @@ public class Main {
                         //whether game is on or not.
                         while (System.currentTimeMillis() < end && check) {
 
-                            //If press LEFT, shape will move to left.
+                            //If press LEFT, tetromino will move to left.
                             if (StdDraw.isKeyPressed(KeyEvent.VK_LEFT)) {
-                                shape.moveLeft(board);
-                                drawAll(board, shape, shapeNext);
+                                tetromino.moveLeft(grid);
+                                drawAll(grid, tetromino, tetrominoNext);
                             }
 
-                            //If press RIGHT, shape will move to right.
+                            //If press RIGHT,  will move to right.
                             else if (StdDraw.isKeyPressed(KeyEvent.VK_RIGHT)) {
-                                shape.moveRight(board);
-                                drawAll(board, shape, shapeNext);
+                                tetromino.moveRight(grid);
+                                drawAll(grid, tetromino, tetrominoNext);
                             }
 
-                            //If press UP, shape will rotate.
+                            //If press UP, tetromino will rotate.
                             else if (StdDraw.isKeyPressed(KeyEvent.VK_UP)) {
-                                shape.rotate(board);
-                                drawAll(board, shape, shapeNext);
+                                tetromino.rotate(grid);
+                                drawAll(grid, tetromino, tetrominoNext);
                             }
 
                             //PRESS 'S' TO STOP THE GAME
@@ -122,6 +122,17 @@ public class Main {
                                 game_on = false;
                                 System.exit(1);
                             }
+                            //restart option just for end of the game
+                            else if (StdDraw.isKeyPressed(KeyEvent.VK_R)) {
+                                //restart the game
+                                restartProtocol(grid);
+
+                                //first shape created for restarted game
+                                tetromino = randomTetromino(allTetrominos);
+                                tetrominoNext = randomTetromino(allTetrominos);
+
+                                x = true;
+                            }
 
                             //time regulation
                             StdDraw.pause(50);
@@ -129,21 +140,21 @@ public class Main {
 
                         //After move/rotate operations, updating game.
                         drawLines();
-                        score += removeLastLine(board);
-                        drawAll(board, shape, shapeNext);
+                        score += removeLastLine(grid);
+                        drawAll(grid, tetromino, tetrominoNext);
                     }
-                    x = shape.getEnd();
+                    x = tetromino.getEnd();
 
-                    score += Duplicate(board);
+                    score += Duplicate(grid);
 
                     StdDraw.pause(100);
-                    DrawBoard(board);
+                    DrawGrid(grid);
                     StdDraw.show();
 
                     if (!x) {
                         break;
                     }
-                    shape = shapeNext;
+                    tetromino = tetrominoNext;
 
                 } while (x);
 
@@ -151,7 +162,7 @@ public class Main {
 
             }
             else{
-                gameEndPage(board);
+                gameEndPage(grid);
             }
 
         }
@@ -159,77 +170,78 @@ public class Main {
 
     /**
      * This function draws all parts of the game to panel.
-     * @param board : game field
-     * @param shape : current shape
-     * @param shapeNext : next shape
+     * @param grid : game field
+     * @param tetromino : current tetromino
+     * @param tetrominoNext : next tetromino
      */
-    static void drawAll(int[][] board, Shape shape, Shape shapeNext){
-        DrawBoard(board);
-        DrawShape(shape);
-        drawNextShape(shapeNext);
+    static void drawAll(int[][] grid, Tetromino tetromino, Tetromino tetrominoNext){
+        DrawGrid(grid);
+        DrawTetromino(tetromino);
+        drawNextTetromino(tetrominoNext);
         StdDraw.show();
     }
 
     /**
-     * This function clear the board to fresh start
-     * @param board: game field we are on
+     * This function clear the grid to fresh start
+     * @param grid: game field we are on
      */
-    static void restartProtocol(int[][] board){
+    static void restartProtocol(int[][] grid){
         //clear all drawings
         StdDraw.clear();
         //restart score
         score = 0;
-        //clear all board fields
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                board[i][j] = 0;
+        //clear all grid fields
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                grid[i][j] = 0;
             }
         }
     }
 
     /**
-     * This function creates first shape to start the game
-     * @param shape1: all shapes that can occur
-     * @return: created new shape
+     * This function creates first tetromino to start the game
+     * @param tetromino1: all tetrominos that can occur
+     * @return: created new tetromino
      */
-    static Shape randomShape(int[][][] shape1){
+    static Tetromino randomTetromino(int[][][] tetromino1){
 
-        //creating shape and define numbers
-        int[][] copyShape = new int[4][2];
+        //creating tetromino and define numbers
+        int[][] copyTetromino = new int[4][2];
 
         //random
         Random rnd = new Random();
 
         //getting random integer up to 7
         int randomNumber = rnd.nextInt(7);
-        copy(copyShape, shape1[randomNumber]);
+        copy(copyTetromino, tetromino1[randomNumber]);
 
-        //main shape with random numbers
-        Shape shape = new Shape(copyShape, 8, 12, randomValues());
-        return shape;
+        //main tetromino with random numbers
+        Tetromino tetromino = new Tetromino(copyTetromino, 8, 12, randomValues());
+        return tetromino;
     }
 
     /**
      * This function shows end game page.
-     * @param board : game board
+     * @param grid : game grid
      */
-    static void gameEndPage(int [][] board){
+    static void gameEndPage(int [][] grid){
 
         //end game page.
         StdDraw.setPenColor(StdDraw.DARK_GRAY);
-        StdDraw.filledRectangle(0, 0, board[0].length, board.length);
+        StdDraw.filledRectangle(0, 0, grid[0].length, grid.length);
 
         //press r to play again.
         StdDraw.setPenColor(StdDraw.YELLOW);
         StdDraw.text(4,3.5, "PRESS 'R' TO RESTART.");
+        StdDraw.text(4,3, "SCORE : "+score);
         StdDraw.show();
     }
 
 
     /**
      *This function gets current's coordinates and assign to old.
-     * @param old coordinates of the shape
-     * @param current coordinates of the shape
+     * @param old coordinates of the tetromino
+     * @param current coordinates of the tetromino
      */
     static void copy(int [][]old,int[][]current){
         for(int i=0; i<old.length; i++)
@@ -239,41 +251,41 @@ public class Main {
     }
 
     /**
-     * This function's purpose is movement of shapes.
-     * @param board
+     * This function's purpose is movement of tetrominos.
+     * @param grid
      * @return : Total number to update score value.
      */
-    static int shiftDown(int [][] board){
+    static int shiftDown(int [][] grid){
         int total = 0;
 
-        //checks whether shape inside the board or not.
-        //Updates coordinates of shapes
+        //checks whether tetromino inside the grid or not.
+        //Updates coordinates of tetrominos
         //Increase score if there is a merge
-        for (int i = 0; i < board.length-1; i++) {
+        for (int i = 0; i < grid.length-1; i++) {
             boolean x =false;
-            for (int j = 0; j < board[i].length; j++) {
-                if(board[i][j]!=0&&board[i+1][j]==0){
+            for (int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j]!=0&&grid[i+1][j]==0){
 
-                    int temp= board[i][j];
-                    board[i][j]=0;
-                    board[i+1][j]=temp;
+                    int temp= grid[i][j];
+                    grid[i][j]=0;
+                    grid[i+1][j]=temp;
                     x=true;
                 }
 
-                else if(board[i][j]!=0&&board[i][j]==board[i+1][j]){
+                else if(grid[i][j]!=0&&grid[i][j]==grid[i+1][j]){
 
-                    board[i+1][j]+=board[i][j];
-                    total+=board[i+1][j];
-                    board[i][j]=0;
+                    grid[i+1][j]+=grid[i][j];
+                    total+=grid[i+1][j];
+                    grid[i][j]=0;
                     x=true;
                 }
             }
             if (x){
                 i=0;
-                DrawBoard(board);
+                DrawGrid(grid);
                 StdDraw.pause(50);
                 StdDraw.show();
-                total+=shiftDown(board);
+                total+=shiftDown(grid);
             }
         }
         return total;
@@ -282,38 +294,38 @@ public class Main {
     /**
      * This function update score.
      * When 2 numbers are merged, update the score.
-     * @param board
+     * @param grid
      * @return : total to update score.
      */
-    public static int Duplicate(int [][] board){
-        int total= shiftDown(board);
+    public static int Duplicate(int [][] grid){
+        int total= shiftDown(grid);
         return total;
     }
 
     /**
-     * This function deletes the last line that completely full by shapes.
-     * @param board : checks whether lines are full or not.
+     * This function deletes the last line that completely full by tetrominos.
+     * @param grid : checks whether lines are full or not.
      * @return : total number to update score.
      */
-    public static int removeLastLine(int[][] board) {
+    public static int removeLastLine(int[][] grid) {
         int total=0;
         boolean isEmpty=false;
         for (int i =0; i < 8 ; i++) {
-            if(board[11][i]==0)
+            if(grid[11][i]==0)
                 isEmpty=true;
         }if(!isEmpty) {
             for (int k =0; k < 8 ; k++) {
-                total +=board[11][k];
-                board[11][k]=0;
+                total +=grid[11][k];
+                grid[11][k]=0;
             }
-            shiftDown(board);
-            removeLastLine(board);
+            shiftDown(grid);
+            removeLastLine(grid);
         }
         return total;
     }
 
     /**
-     * This function gives random values to square parts of the shape.
+     * This function gives random values to square parts of the tetromino.
      * 1/4 possibility for "4" and 3/4 possibility for "2".
      * @return : array that keep random number
      */
@@ -328,7 +340,7 @@ public class Main {
     }
 
     /**
-     * This function draw base lines of board
+     * This function draw base lines of grid
      */
     public static void drawLines() {
 
@@ -367,35 +379,38 @@ public class Main {
         StdDraw.setPenColor(StdDraw.RED);
         StdDraw.text(9, 4.5, "EXIT -> 'E'");
 
+        StdDraw.setPenColor(StdDraw.PRINCETON_ORANGE);
+        StdDraw.text(9, 5, "RES -> 'R'");
+
         StdDraw.setPenColor(StdDraw.WHITE);
         StdDraw.text(9, 1.5, Integer.toString(score));
         StdDraw.text(9, 9.5, "NEXT");
     }
 
     /**
-     * This function draws board.
-     * @param board : coordinates to draw board properly
+     * This function draws grid.
+     * @param grid : coordinates to draw grid properly
      */
-    static void DrawBoard(int[][] board){
+    static void DrawGrid(int[][] grid){
 
-        //Draw basis of the board with lines.
+        //Draw basis of the grid with lines.
         drawLines();
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
 
-                //checks whether there is a shape or not.
-                if(board[i][j]!=0){
+                //checks whether there is a tetromino or not.
+                if(grid[i][j]!=0){
 
-                    //if there is a shape with number, this replicate that number's color.
+                    //if there is a tetromino with number, this replicate that number's color.
                     //With this, we don't lose previous statements.
-                    setColor(board[i][j]);
+                    setColor(grid[i][j]);
                     StdDraw.filledSquare(j + 0.48, i + 0.48, 0.48);
                     StdDraw.setPenColor(StdDraw.BLACK);
-                    StdDraw.text(j + 0.48, i + 0.48, Integer.toString(board[i][j]));
+                    StdDraw.text(j + 0.48, i + 0.48, Integer.toString(grid[i][j]));
                 }
 
-                //if board's specific square is empty, color that piece as background
+                //if grid's specific square is empty, color that piece as background
                 else{
                     StdDraw.setPenColor(95,158,60);
                     StdDraw.filledSquare(j + 0.48, i + 0.48, 0.48);
@@ -405,16 +420,16 @@ public class Main {
     }
 
     /**
-     * This function draws shape to the board.
-     * @param shape : current shape
+     * This function draws tetromino to the grid.
+     * @param tetromino : current tetromino
      */
-    static void DrawShape(Shape shape){
+    static void DrawTetromino(Tetromino tetromino){
 
-        //Firstly, get shape's coordinates.
-        int [][] arr = shape.getCoord();
+        //Firstly, get tetromino's coordinates.
+        int [][] arr = tetromino.getCoord();
 
-        //Then, values of that shape
-        int [] values = shape.values;
+        //Then, values of that tetromino
+        int [] values = tetromino.values;
 
         //Draw them piece by piece
         for (int i = 0; i < 4; i++) {
@@ -432,14 +447,14 @@ public class Main {
     }
 
     /**
-     * This function gets Shape object to draw next shape.
-     * @param shape : next shape
+     * This function gets Tetromino object to draw next tetromino.
+     * @param tetromino : next tetromino
      */
-    public static void drawNextShape(Shape shape) {
+    public static void drawNextTetromino(Tetromino tetromino) {
 
-        //assign next shape to new int array.
+        //assign next tetromino to new int array.
         //.view() method gives coordinates + numbers combined.
-        int [][] arr=shape.view();
+        int [][] arr= tetromino.view();
 
         //checks all lines of arr and print them to proper place.
         for (int i = 0; i < arr.length; i++) {
@@ -478,7 +493,7 @@ public class Main {
     }
 
     /**
-     * This function is all about colors of numbers that we are using in shape.
+     * This function is all about colors of numbers that we are using in tetromino.
      * @param a : number to define it's color.
      */
     public static void setColor(int a){
---
+++ Tetromino.java
@@ -0,0 +1,293 @@
+public class Tetromino {
+
+    //values of boxes, 2, 4, 8, etc...
+    int values [];
+
+    // x and y coordinates of each boxes.
+    int coord[][];
+
+    //x coordinates stored in the first index, y coordinate are stored in the second index
+    // y length of the grid
+    int boardY;
+
+    // x length of the grid
+    int boardX;
+
+    // if it's false, game ends
+    private boolean end=true;
+
+    /**
+     * constructor method of Tetromino class
+     * @param coord coordinates
+     * @param boardX X length of the grid
+     * @param boardY Y length of the grid
+     * @param values values of boxes
+     */
+    public Tetromino(int[][] coord, int boardX, int boardY, int[] values){
+        this.coord=coord;
+        this.boardX=boardX;
+        this.boardY=boardY;
+        this.values=values;
+
+    }
+
+    /**
+     * @param board is grid
+     * @return if there is any suitable place to move, it returns false, else it return true
+     *
+     * Function move the tetrominoes downward by 1 in the y axis.
+     * it decreases y coordinates of tetorominoes by 1
+     */
+    boolean move(int [][]board){
+
+        //if it's suitable to move down it move, else it's stopped and grid is updated
+
+        if (canMoveDownward(board)){
+            for (int i = 0; i < coord.length; i++) {
+                coord[i][1]+=1;
+
+            }
+            return false;
+        }
+        else{
+            done(board);
+            return true;
+        }
+    }
+
+    /**
+     * The function checks if pieces can move downward
+     * @param board
+     * @return
+     */
+    boolean canMoveDownward(int [][] board){
+        for(int i = 0; i < coord.length;i++){
+            if(coord[i][0]<0|| coord[i][1]<0){
+                continue;
+            }
+            if( coord[i][1]+1>board.length-1)
+                return false;
+
+            if(board[coord[i][1]+1][coord[i][0]]!=0)
+                return false ;
+        }
+        return true;
+    }
+
+    /**
+     * This function updates the grid
+     * @param board is grid
+     */
+    void done(int [][]board){
+
+        //if the player lose the game it doesn't update
+
+        if(!lose()) {
+            for (int i = 0; i < coord.length; i++) {
+                board[coord[i][1]][coord[i][0]] = values[i];
+            }
+        }
+        else {
+
+            //if lose make end false
+            end=false;
+        }
+
+    }
+
+    /**
+     * it moves the tetromino to the right
+     * @param board is grid
+     */
+    void moveRight(int[][] board){
+
+        //checks if it goes out of the grid
+
+        if(!greaterThanX(board))
+            for (int i = 0; i < coord.length; i++)
+                coord[i][0]+=1;
+
+    }
+
+    /**
+     * it moves the tetromino to the left
+     * @param board is grid
+     */
+    void moveLeft(int[][]board){
+
+        //checks if it goes out of the grid
+
+        if(!lessThanX(board)){
+            for (int i = 0; i < coord.length; i++) {
+                coord[i][0]-=1;
+            }
+        }
+    }
+
+    /**
+     * checks if it suitable to move left
+     * @param board
+     * @return true if it can't move, else false
+     */
+    boolean lessThanX(int[][] board){
+
+        // it checks if there is any tetromino on the left or it goes out of the grid
+
+        for(int i = 0; i < coord.length;i++){
+            if(coord[i][0]-1<0){
+                return true;
+            }
+
+            if(coord[i][1]-1<0){
+                continue;
+            }
+
+            if(board[coord[i][1]][coord[i][0]-1]!=0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * checks if it suitable to move right
+     * @param board
+     * @return true if it can't move, else false
+     */
+    boolean greaterThanX(int[][] board){
+
+        // it checks if there is any tetromino on the right or it goes out of the grid
+
+        for(int i = 0; i < coord.length;i++){
+            if(coord[i][0]+1>boardX-1 ){
+                return true;
+            }
+
+            if(coord[i][1]<0) {
+                continue;
+            }
+            if(board[coord[i][1]][coord[i][0]+1]!=0 ){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * it y coordinates goes out of the range of the grid it loses
+     * @return true if it lose , false if it's not
+     */
+    boolean lose(){
+        for (int i = 0; i < coord.length; i++) {
+            if(coord[i][1]<0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * getter function of end
+     * @return end
+     */
+    boolean getEnd() {
+        return end;
+    }
+
+    /**
+     * find minimum y coordinate in the coordinates array
+     * @return the minmum coordinate
+     */
+    int minY(){
+        int min= 20;
+        for (int i = 0; i < coord.length; i++) {
+            if(coord[i][1]<min){
+                min=coord[i][1];
+            }
+        }
+        return min;
+
+    }
+
+    /**
+     * find minimum x coordinate in the coordinates array
+     * @return the minmum coordinate
+     */
+    int minX(){
+        int min=20;
+        for (int i = 0; i < coord.length; i++) {
+            if(coord[i][0]<min){
+                min=coord[i][0];
+            }
+        }
+        return min;
+    }
+
+    /**
+     * it rotates the tetromino if it's suitable to move
+     * @param board
+     * @return if one of the box goes out of grid or duplicate with another tetrominoe
+     * it returns false and quits the function
+     */
+    boolean rotate(int [][] board){
+        // function accepts one of the coordinates as middle points and rotates the array around of it
+        int midX=coord[1][0];
+        int midY=coord[1][1];
+        //middle points
+        int [][] temp = new int[4][2];
+        //temp array for coordinates
+        for (int i = 0; i < 4; i++) {
+            //moves the coordinate the 0,0 scale and rotate 90 degree clockwise
+            temp[i][1] = -(coord[i][0] - midX) + midY;
+            temp[i][0] = (coord[i][1] - midY) + midX;
+            //if it can't rotates false returned
+            if(temp[i][0]<0 || temp[i][0]>= boardX || temp[i][1]>=boardY){
+                return false;
+            }
+            if (temp[i][1]<0)
+                continue;
+            if(board[temp[i][1]][temp[i][0]]!=0 )
+                return false;
+
+
+        }
+        coord=temp;
+        return true;
+    }
+
+    /**
+     * it views the tetromino
+     * first it finds minimums indexes and decreases every single coordinates by these
+     * and puts the tetromino in a 2x4 matrix and return it
+     * @return rew, a 2x4 matrix, view of the tetromino
+     */
+    int [][] view(){
+        int [][] arr = new int [4][2];
+        int min = minY();
+        int min2=minX();
+        //min could be negative at the first
+        if(min<0)
+            min=min*-1;
+        //decrease coordinates
+        for (int i = 0; i < arr.length; i++) {
+            arr[i][0]=coord[i][0]-min2;
+            arr[i][1]=min+coord[i][1];
+        }
+        //and fill the rew array using coordinates
+        int [][] rew = new int [4][4];
+        for (int i = 0; i <arr.length ; i++) {
+            rew[arr[i][1]][arr[i][0]]=values[i];
+        }
+        return rew;
+    }
+
+    /**
+     * getter function of coordinates array
+     * @return coord
+     */
+    public int[][] getCoord() {
+        return coord;
+    }
+
+
+}
